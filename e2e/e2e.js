f7c4853c1e47deeca39513c68a57ca91ed2a7da1
Romaric Philogène <rphilogene@qovery.com>
2023-08-28 10:02:16 +0200

    wip: workflow

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -1,31 +1,30 @@
 import http from 'k6/http';
-import { check, group, sleep, fail } from 'k6';
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import {check, group, sleep, fail} from 'k6';
+import {uuidv4} from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 const api_host = `${__ENV.API_HOST}/api`;
-
 export const options = {
-    stages: [
-        { duration: '5m', target: 100 }, // traffic ramp-up from 1 to 100 users over 5 minutes.
-        //{ duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
-        { duration: '1m', target: 50 }, // ramp-down to 50 users
-    ]
+  stages: [
+    {duration: '5m', target: 100}, // traffic ramp-up from 1 to 100 users over 5 minutes.
+    //{ duration: '30m', target: 100 }, // stay at 100 users for 30 minutes
+    {duration: '1m', target: 50}, // ramp-down to 50 users
+  ]
 }
 
 export function setup() {
-    // add some data
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
+  // add some data
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-    for (let i = 0; i < 20; i++) {
-        const res = http.post(api_host, JSON.stringify({title: uuidv4()}), params);
-        check(res, {'item added': (r) => r.status === 201});
-    }
+  for (let i = 0; i < 20; i++) {
+    const res = http.post(api_host, JSON.stringify({title: uuidv4()}), params);
+    check(res, {'item added': (r) => r.status === 201});
+  }
 }
 
 export default function () {
-    http.get(api_host);
+  http.get(api_host);
 }
